868d19cf643ca261b21f3880b37afdfd5f123576
nicomp42 <nicholdw@ucmail.uc.edu>
2024-03-07 05:39:18 -0500

    First Commit

---
+++ IS4010-001-Spring-2024-Midterm-Exam-Part-02a/catPackage/catClass.py
@@ -0,0 +1,21 @@
+
+
+# 1. Complete the sayMeow method
+# 2. Complete the getName method
+class Cat:
+    def __init__(self, name):
+        self.name = name
+    
+    def sayMeow(self):
+        '''
+        Print meow to the console
+        '''
+        pass
+    
+    def getName(self):
+        '''
+        Get the name of the Cat object
+        @return: The name of the Cat object
+        '''
+        pass
+    
---
+++ IS4010-001-Spring-2024-Midterm-Exam-Part-02a/mainPackage/main.py
@@ -0,0 +1,13 @@
+
+from catPackage.catClass import Cat
+
+if __name__ == "__main__":
+    print("Test the Cat class...")
+    myCat = Cat("Violet")
+    myCat.sayMeow()
+    print(myCat.getName())
+    
+    
+    # 3. Instantiate a Cat object, demonstrate the sayMeow and the getName methods
+
+    
